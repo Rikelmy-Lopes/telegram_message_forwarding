16fbd9ebc0a2828ca0e7fb778c89f3bea4068d8a
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-07-04 12:54:54 -0300

    melhorias na interação com o bot

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -6,8 +6,8 @@ from telegram.ext import CommandHandler, ContextTypes, ConversationHandler
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     commands_text = (
         r"🤖 *Comandos Disponíveis:*" "\n\n"
-        r"🔹 `/words` \- Manipular as palavras filtradas" "\n"
-        r"🔹 `/channels` \- Manipular quais canais devem ser ouvidos" "\n\n"
+        r"🔹 /words \- Manipular as palavras filtradas" "\n"
+        r"🔹 /channels \- Manipular quais canais devem ser ouvidos" "\n\n"
         r"💡 _Dica: Digite /cancel a qualquer momento para sair\._"
     )
     await update.message.reply_text(commands_text, parse_mode='MarkdownV2')
@@ -19,4 +19,4 @@ start_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start_command)],
     states={},
     fallbacks=[]
-)
+)   
