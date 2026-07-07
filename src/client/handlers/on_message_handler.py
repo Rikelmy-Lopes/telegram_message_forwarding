f72b9c9f6ee0a613d64a702c06ebe79258b204c2
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-07-07 09:43:54 -0300

    minor changes

--- src/client/handlers/on_message_handler.py
+++ src/client/handlers/on_message_handler.py
@@ -1,4 +1,6 @@
 import logging
+
+from telethon import events
 from bot.messages.message import send_message
 from config.state import TELEGRAM_FILTER
 # from utils import send_notification
@@ -6,22 +8,22 @@ from config.state import TELEGRAM_FILTER
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-async def on_new_messages(event):
+async def on_new_messages(event: events.NewMessage.Event):
     try:
         texto_mensagem: str = event.message.text
 
         if not texto_mensagem:
             return
-
-        if not event.chat:
-            return
+        
+        chat_title = event.chat.title if event.chat else 'Chat Desconhecido'
+        message_id = event.message.id
 
         for palavra in TELEGRAM_FILTER.get_words():
             if palavra in texto_mensagem.lower():
-                logger.info(f"Palavra encontrada no canal {event.chat.title}!")
+                logger.info(f"Palavra encontrada no canal {chat_title}!")
                 
-                link_mensagem = f"https://t.me/{event.chat.username}/{event.message.id}" if event.chat.username else "Canal Privado"
-                alerta = f"🚨 **Palavra-chave detectada! ({palavra})** \n\nCanal: {event.chat.title}\nTexto: {texto_mensagem}\nLink: {link_mensagem}"
+                link_mensagem = f"https://t.me/{event.chat.username}/{message_id}" if event.chat and event.chat.username else "Canal Privado"
+                alerta = f"🚨 **Palavra-chave detectada! ({palavra})** \n\nCanal: {chat_title}\nTexto: {texto_mensagem}\nLink: {link_mensagem}"
                 
                 await send_message(alerta)
                 # send_notification(alerta, palavra)
