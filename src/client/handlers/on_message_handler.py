0cb6989c36119355d6842b98bb4442af33ef758f
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-09-08 07:46:02 -0300

    minor changes

--- src/client/handlers/on_message_handler.py
+++ src/client/handlers/on_message_handler.py
@@ -1,5 +1,4 @@
 import logging
-import time
 from telethon import events
 from bot.messages.message import send_message
 from config.state import STATE
@@ -13,20 +12,20 @@ _TELEGRAM_FILTER = STATE.get_telegram_filter()
 
 async def on_new_messages(event: events.NewMessage.Event):
     try:
-        texto_mensagem: str = event.message.text
+        text_message: str = event.message.text
 
-        if not texto_mensagem:
+        if not text_message:
             return
         
         chat_title = event.chat.title if event.chat and event.chat.title else 'Chat Desconhecido'
         message_id = event.message.id
 
-        texto_comparacao = normalize_text(texto_mensagem, True)
+        comparison_text = normalize_text(text_message, True)
 
         for word_filter in _TELEGRAM_FILTER.get_word_filters():
             words = word_filter.get_value()
 
-            is_all_finded = all(contains_word(word, texto_comparacao) for word in words)
+            is_all_finded = all(contains_word(word, comparison_text) for word in words)
 
             if is_all_finded:
                 words_str = ', '.join(words)
@@ -34,9 +33,9 @@ async def on_new_messages(event: events.NewMessage.Event):
                 logger.info(f"Palavra encontrada no chat {chat_title} - ({words_str})!")
                     
                 link_mensagem = f"https://t.me/{event.chat.username}/{message_id}" if event.chat and event.chat.username else "Chat Privado"
-                alerta = f"🚨 <b>Palavra-chave detectada! ({words_str})</b> \n\nChat: {chat_title}\nTexto: {texto_mensagem}\nLink: {link_mensagem}"
+                alert_message = f"🚨 <b>Palavra-chave detectada! ({words_str})</b> \n\nChat: {chat_title}\nTexto:\n{text_message}\n\nLink da mensagem: {link_mensagem}"
             
-                await send_message(alerta)
+                await send_message(alert_message)
                 break
 
     except Exception as e:
